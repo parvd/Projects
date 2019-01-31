227d4f51004833c1db1550421c3551f47720294a
parvd <parvd5@gmail.com>
2019-02-01 00:43:13 +0530

    Time-Convert

---
+++ convert1.cpp
@@ -0,0 +1,84 @@
+/*
+Tony Strac is in space right now and he want to know the time on earth but he cannot see time
+beacuse his watch is damage.Nebula can repair the watch with his robotic hand.But there is problem
+when nebula repairs the watch clocks format gets change from 12-Hr format to 24-hr format and also
+she takes approx. 2hrs to finish repair work in that watch stop working for that 2hr.
+
+Now you have to help tony for resting watch time because nebula can't reset it with earth time.
+
+Note: Output will be in 24-hr format.
+
+Sample: Test Case
+
+07:45:02PM
+21:45:02
+
+Explantion:
+
+07:45:02PM is in 12-hr Format
+19:45:02  is in 24-hr Format
+And 2 extra hrs for repairing of watch.
+*/
+#include<iostream>
+using namespace std;
+
+void convert(string str)
+{
+	int h1 = (int)str[1] - '0';
+	int h2 = (int)str[0] - '0';
+	int hh = (h2 * 10 + h1 % 10);
+
+	// If time is in "AM"
+	if (str[8] == 'A')
+	{
+	if ((hh) == 12)
+		{
+		    hh=00;
+		}
+
+	}
+
+	// If time is in "PM"
+	else
+	{
+		if (hh == 12)
+		{
+			hh=12;
+		}
+		else
+		{
+			hh = hh + 12;
+		}
+	}
+	//cout<<"\n";
+	int r=hh+2;
+	if(r>24)  //if it exceed 24hr
+    {
+        int x=x-24;
+        cout<<x;
+        for (int i=2; i <= 7; i++)
+				cout << str[i];
+    }
+    else
+    {
+        cout<<r;
+        for (int i=2; i <= 7; i++)
+				cout << str[i];
+
+        cout<<endl;
+    }
+}
+
+int main()
+{
+int t;
+cin>>t;
+cin.ignore();
+while(t--)
+{
+string str;
+getline(cin,str);
+convert(str);
+}
+}
+
